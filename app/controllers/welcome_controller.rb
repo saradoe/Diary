da7d9bccf5d0ea5e58b1c657730aa05d9aa2cfd7
saradoe <sararonzerov@gmail.com>
2015-03-29 12:01:24 +0200

    Model created

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @entries = Entry.all
+		@website = [
+			["http://tryruby.org/levels/4/challenges/3", "Try Ruby"],
+			["http://wuaki.tv", "Where I spend 80% of my life"],
+			["http://twitter.com/sara_doe", "Where I tell my life to strangers"],
+			["http://instagram.com/sara_doe", "Where I SPAM everyone with pics of my cat"]
+		]
   end
 end
